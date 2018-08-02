f87c9188cc73de015d11e0838d44800f36a894b0
WaniFi <skelpd24@gmail.com>
2018-08-02 17:54:55 +0900

    2312 코드 구현

--- baekjoon_2312/main.cpp
+++ baekjoon_2312/main.cpp
@@ -3,5 +3,24 @@ using namespace std;
 
 int main()
 {
-	
+	int c, i1, j;
+	int num_count = 0;
+	cin >> c;
+	for (int i = 0; i < c; i++) {
+		cin >> i1;
+		j = 2;
+		while (true) {
+			if (i1%j == 0) {
+				num_count++;
+				i1 /= j;
+				continue;
+			}
+			else if (num_count != 0) {
+				cout << j << " " << num_count << endl;
+				num_count = 0;
+				if (i1 == 1) break;
+			}
+			j++;
+		}
+	}
 }
